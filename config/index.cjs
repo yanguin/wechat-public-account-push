2adfc5b2d85cc04a3b58b828691899e563fdd326
yanguin <141069219+yanguin@users.noreply.github.com>
2023-08-01 03:41:14 +0800

    index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ZnzJ4ZKw6urfWGoE8xu49IuhYTcwcbJ5X58t6PVuN-4',
+      id: 'ognbQ6o9y3B6DUuDvXCBqxfiXJdU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'fJjEyKwGBy35e7JqhFGVG_EUTe3oo0HXh8sm0djseaA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
