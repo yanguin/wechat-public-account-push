03223b30c56b97ed391c40ee3844ebefa8fc609e
yanguin <141069219+yanguin@users.noreply.github.com>
2023-08-01 04:01:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ognbQ6o9y3B6DUuDvXCBqxfiXJdU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'hrcMDixM9-3GXm5e2BpulvRgK7699tlC9By6w2lybdg',
+      useTemplateId: '6Bk8kNJzG31L44T9ijyKvfzUItAq0x6QOxY9uouBkG8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-27',
       festivals: [
